8ef43baebe23394953113b6bb91edc5273599d0b
AnTeCP100 <36754567+AnTeCP100@users.noreply.github.com>
2021-05-19 12:06:21 +0800

    0519

---
+++ 860/sln.c
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <string.h>
+
+#include "../method.h"
+bool lemonadeChange(int* bills, int billsSize){
+    bool result = false;
+    int transArr[billsSize][2],addResult;
+    for(int locate = 0;locate<billsSize;++locate){
+        
+    }
+    return result;
+}
+
+int IMBIG(){
+    int billsArr[5]= {5,5,5,10,20};
+    
+    int *bills = billsArr;
+    
+    return 0;
+}
