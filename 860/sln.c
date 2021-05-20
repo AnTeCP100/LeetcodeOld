d46fde1112fae3d6da7b230644e43640f804686c
AnTeCP100 <36754567+AnTeCP100@users.noreply.github.com>
2021-05-20 18:39:01 +0800

    0520

--- 860/sln.c
+++ 860/sln.c
@@ -1,13 +1,45 @@
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 
 #include "../method.h"
 bool lemonadeChange(int* bills, int billsSize){
-    bool result = false;
-    int transArr[billsSize][2],addResult;
-    for(int locate = 0;locate<billsSize;++locate){
-        
+    bool result = true;
+    //5,10,15
+    int _wallet[3]={0,0,0};
+    for(int count=0;count<billsSize;++count){
+        switch (bills[count])
+        {
+        case 5:
+            ++_wallet[0];
+            break;
+        case 10:
+            if(_wallet[0]<1){
+                result = false;
+            }
+            --_wallet[0];
+            ++_wallet[1];
+            break;
+        case 20:
+            if(_wallet[1]>0 && _wallet[0]>0){
+                --_wallet[1];
+                --_wallet[0];
+            }else if (_wallet[0]>=3){
+                _wallet[0]=_wallet[0]-3;
+            }else{
+                result =false;
+            }
+            break;     
+        default:
+            //printf("wrong number");
+            break;
+        }
+
+        if(!result){
+            break;
+        }
     }
+
     return result;
 }
 
@@ -15,6 +47,7 @@ int IMBIG(){
     int billsArr[5]= {5,5,5,10,20};
     
     int *bills = billsArr;
-    
+    bool result = lemonadeChange(bills,5);
+    printf("%d",result);
     return 0;
 }
