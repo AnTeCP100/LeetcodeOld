620ebb42790f8d8f34d71a5fb4278404b3bf544d
AnTeCP100 <36754567+AnTeCP100@users.noreply.github.com>
2021-10-06 10:27:54 +0800

    [211006] fix

--- 86/86.js
+++ 86/86.js
@@ -5,11 +5,6 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
- function ListNode(val, next)
- {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
- }
 
 /**
  * @param {ListNode} head
