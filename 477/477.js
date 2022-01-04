e398f04a9d9b0e53d1738ec19d2de023c5429208
AnTeCP100 <36754567+AnTeCP100@users.noreply.github.com>
2022-01-04 20:36:07 +0800

    220104

--- 477/477.js
+++ 477/477.js
@@ -40,34 +40,54 @@
 };
 
 
-var totalHammingDistance = function(nums) {
+var totalHammingDistance_fail = function(nums) {
     //two error more than nine and too log will broke
     var count = nums.length;
-    var total = 0;
     var bitStrArr = [];
-    var bitStrBigArr = [];
+    var bitCountArr = [];
     var ret = 0;
 
     //change to byte
     nums.forEach(element => {
         bitStrArr.push(element.toString(2));
     });
-    //console.log(arr)
+    console.log(bitStrArr)
 
     //transform digti and reverse the number 
     //foreach in arr  
     //split the bit string 
     //then covert to number and add to bitArr sam location if bitArr not exist add to new array
     //14 = "1110" => [1,1,1,0]
-    //bitArr = [0,0,0,0]
-    //       + [0,1,1,1]
+    //bitStrBigArr = [0,0,0,0]
+    //             + [0,1,1,1]
 
+    bitStrArr.forEach(element=>{
+        //split the bit string 
+        var cur_bit_string_arr = element.split('');
+        
+        
+        //covert to int and add to bitCountArr
+        for(var i = cur_bit_string_arr.length-1;i>=0;i--)
+        {
+            //count now bitCountArr pos
+            var pos = cur_bit_string_arr.length-1-i;
+
+            console.log(cur_bit_string_arr[i])
+            //check or init bitCountArr[i]
+            if(bitCountArr[pos]==undefined)
+            {
+                bitCountArr[pos]=0;
+            }
+            bitCountArr[pos]+=parseInt(cur_bit_string_arr[i]);         
+        }
+    })
+    console.log(bitCountArr)
 
     //C(count,2)
     var total_count = (count*(count-1))/2;
 
     //count every digital
-    bitArr.forEach(e=>{
+    bitCountArr.forEach(e=>{
         if(e!=0 && e<count)
         {
             ret+=(total_count- (e*(e-1))/2 - ((count-e)*(count-e-1))/2)
@@ -76,7 +96,8 @@ var totalHammingDistance = function(nums) {
     return ret
 };
 
-var nums = [4,14,4,14]
+
+var nums = [4,14,2]
 console.log(totalHammingDistance(nums))
 
 
